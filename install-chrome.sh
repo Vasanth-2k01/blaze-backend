b1ccf67148c261aab17e7d380002e4072cbc29aa
m_vasanth <vasanth260901@gmail.com>
2024-12-03 01:57:08 +0530

    sh updated

--- install-chrome.sh
+++ install-chrome.sh
@@ -4,10 +4,10 @@
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 # Install the downloaded package (requires sudo)
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
 
 # Clean up the downloaded .deb file
 rm google-chrome-stable_current_amd64.deb
 
 # Print the Chrome version to verify installation
-google-chrome --version
+# google-chrome --version
