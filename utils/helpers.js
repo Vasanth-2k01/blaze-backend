df10bfbf721142d6afc820a3dbe36d9da4e5c669
m_vasanth <vasanth260901@gmail.com>
2024-12-02 23:52:53 +0530

    puppeteer chrome path changed

--- utils/helpers.js
+++ utils/helpers.js
@@ -241,7 +241,7 @@ module.exports = {
       }
       // const browser = await puppeteer.launch();
       const browser = await puppeteer.launch({
-        executablePath: '/opt/render/local/bin/google-chrome', 
+        // executablePath: '/opt/render/local/bin/google-chrome', 
         args: ['--no-sandbox', '--disable-setuid-sandbox'],  
       });
       const page = await browser.newPage();
