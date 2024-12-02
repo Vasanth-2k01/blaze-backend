008e8f63384483baec04143af0161219565ea415
m_vasanth <vasanth260901@gmail.com>
2024-12-02 23:10:15 +0530

    sh file changes

--- install-chrome.sh
+++ install-chrome.sh
@@ -10,15 +10,15 @@ ar x google-chrome-stable_current_amd64.deb
 tar -xf data.tar.xz
 
 # Move binaries to local bin
-mkdir -p ~/local/bin
-cp -r ./opt/google/chrome/* ~/local/bin/
+mkdir -p /usr/bin/google-chrome
+cp -r ./opt/google/chrome/* /usr/bin/google-chrome/
 
 # Add Chrome to PATH
-echo 'export PATH=$PATH:~/local/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/bin/google-chrome' >> ~/.bashrc
 source ~/.bashrc
 
 # Cleanup
 cd ~
 rm -rf ~/chrome-install
 
-echo "Google Chrome installed in ~/local/bin"
+echo "Google Chrome installed in /usr/bin/google-chrome"
